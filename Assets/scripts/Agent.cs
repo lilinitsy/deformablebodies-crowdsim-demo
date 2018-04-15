6528f1632f6a92b420b70e3e9040cf15521931d2
ville <lilinitsy@gmail.com>
2018-04-15 18:27:57 -0500

    Creating basis of the AI implementation

---
+++ Assets/scripts/Agent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TO BE ATTACHED TO THE CROWD CLASS
+// SHOULD NOT INTERNALLY REFERENCE CROWD CLASS IN ANY WAY 
+
+
+public class Agent : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start() 
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update() 
+    {
+		
+	}
+}
---
+++ Assets/scripts/Crowd.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// SHOULD HAVE AN AIBehaviour
+// SHOULD HAVE WHATEVER C#'s VECTOR IS OF AGENTS
+
+public class Crowd : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start() 
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update() 
+    {
+		
+	}
+}
