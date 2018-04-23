0a2c894178225288d2944f6746b63ef1dc93f90b
ville <lilinitsy@gmail.com>
2018-04-23 15:13:54 -0500

    Started framework for deformable body

---
+++ Assets/scripts/DeformableBody.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeformableBody : MonoBehaviour {
+	public Vector3 position;
+	public Vector3[] new_vertices;
+	public Vector2[] new_uv;
+	public int[] new_triangles;
+
+
+	// Use this for initialization
+	void Start() 
+	{
+		transform.position = position;
+
+		Mesh mesh = new Mesh();
+		GetComponent<MeshFilter>().mesh = mesh;
+		mesh.vertices = new_vertices;
+		mesh.uv = new_uv; // not gonna fuck with textures for now
+		mesh.triangles = new_triangles;
+	}
+	
+	// Update is called once per frame
+	void Update() 
+	{
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		Vector3[] vertices = mesh.vertices;
+		Vector3[] normals = mesh.normals;
+
+		for(int i = 0; i < vertices.Length; i++)
+		{
+			vertices[i] += normals[i] * Mathf.Sin(Time.time); // just to test
+		}
+
+		mesh.vertices = vertices;
+	}
+
+	void to_string()
+	{
+
+	}
+}
---
+++ Assets/scripts/PointMass.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointMass : MonoBehaviour 
+{
+	public Vector3 position;
+
+	private Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start() 
+	{
+		transform.position = position;
+		rigidbody.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update() 
+	{
+		
+	}
+
+	void to_string()
+	{
+		Debug.Log("Position: " + transform.position.ToString("F8") + "\t Velocity: " + rigidbody.velocity.ToString());
+	}
+}
