5a78e50106f9de6c915d2bf2d0e10634289250d9
ville <lilinitsy@gmail.com>
2018-04-29 22:35:13 -0500

    This will be a backup for the current sound-deformation system. It doesn't really work, and the jelly drifts.

--- Assets/scripts/DeformableBody.cs
+++ Assets/scripts/DeformableBody.cs
@@ -8,6 +8,7 @@ public class DeformableBody : MonoBehaviour {
 	public AudioSource audio_source; // DO NOT attach an audiosource to the jelly; attach it here so I can get a distance
 
 	public Vector3 position;
+	public Vector3 average_position;
 	public Vector3[] new_vertices;
 	public Vector2[] new_uv;
 	public int[] new_triangles;
@@ -21,7 +22,8 @@ public class DeformableBody : MonoBehaviour {
 	public int height; // y
 	public int depth; // z
 
-	private float[] spectrum;
+	private float[] spectrum_samples;
+	private float[] frequency_groups;
 	private List<PointMass> point_mass_list;
 	private PointMass[ , , ] tmp_intermediate_pointmass;
 	private PointMass[ , , ] old_pointmass;
@@ -40,7 +42,7 @@ Also might want to consider Implicit Euler instead.
 		// before making the mesh, 
 		// get the vertices from all the child pointmasses or some shit
 // 			Agent tmp_agent = Instantiate(agent_prefab, agent_position, Quaternion.identity) as Agent;
-
+		average_position = new Vector3(0, 0, 0);
 
 		Mesh mesh = new Mesh();
 		GetComponent<MeshFilter>().mesh = mesh;
@@ -53,7 +55,8 @@ Also might want to consider Implicit Euler instead.
 		old_pointmass = new PointMass[width, height, depth];
 		point_mass = new PointMass[width, height, depth];
 
-		spectrum = new float[256];
+		spectrum_samples = new float[256];
+		frequency_groups = new float[8];
 		song = audio_source.clip;
 
 		init();
@@ -76,15 +79,20 @@ Also might want to consider Implicit Euler instead.
 
 		mesh.vertices = vertices;
 
-		float volume = 0.0f;
-		audio_source.GetOutputData(spectrum, 0);
-
-		for(int i = 0; i < 256; i++)
+		for(int i = 0; i < width; i++)
 		{
-
-			volume += Mathf.Abs(spectrum[i]);
+			for(int j = 0; j < height; j++)
+			{
+				for(int k = 0; k < depth; k++)
+				{
+					average_position += point_mass[i, j, k].position;
+				}
+			}
 		}
-		Debug.Log("Volume: " + volume);
+
+		average_position /= width * height * depth;
+		gameObject.transform.GetChild(0).transform.position = average_position;
+
 
 		// like cloth now...
 
@@ -93,11 +101,11 @@ Also might want to consider Implicit Euler instead.
 
 	private void init()
 	{
-		for(int i = 0; i < width; i++)
+		for(int i = -1 * width / 2; i < width / 2; i++)
 		{
-			for(int j = 0; j < height; j++)
+			for(int j = -1 * width / 2; j < height / 2; j++)
 			{
-				for(int k = 0; k < depth; k++)
+				for(int k = -1 * width / 2; k < depth / 2; k++)
 				{
 					PointMass point_mass = Instantiate(pointmass_prefab, 
 											new Vector3(0, 0, 0),
@@ -212,12 +220,166 @@ Also might want to consider Implicit Euler instead.
 					float v1 = Vector3.Dot(e, old_pointmass[i, j, k].rb.velocity);
 					float v2 = Vector3.Dot(e, old_pointmass[i, j, k + 1].rb.velocity);
 					float force = -1.0f * k_spring * (rest_length - l) - k_dampening * (v1 - v2);
+					Debug.Log("Sample z force: " + force.ToString("F8"));
 
 					point_mass[i, j, k].rb.velocity += (force / mass) * e * delta_time;
 					point_mass[i, j, k + 1].rb.velocity -= (force / mass) * e * delta_time;
 				}
 			}
 		}
+
+		// do this here so it's done only once per frame
+		// I think GetSpectrumData holds frequencies? ie notes
+		/* 	44100 hz song
+			256 samples
+
+			Can make different bands?
+
+		
+		*/
+		audio_source.GetSpectrumData(spectrum_samples, 0, FFTWindow.Rectangular);
+		determine_frequency_groups();
+		
+
+		
+		// sound forces - will do by octants, I suppose?
+
+		// octant 0, front lower left
+		for(int i = 0; i < width / 2; i++)
+		{
+			for(int j = 0; j < height / 2; j++)
+			{
+				for(int k = 0; k < depth / 2; k++)
+				{
+					Vector3 e = audio_source.transform.position - point_mass[i, j, k].position;
+					float l = Vector3.Magnitude(e); // confirmed functioning
+					e.Normalize();
+
+					float force = sound_force_calculations(point_mass[i, j, k].position, frequency_groups[0], l);
+					point_mass[i, j, k].rb.velocity +=  (force / mass) * e * delta_time;
+				}
+			}
+		}
+	
+		// octant 1, back lower left
+		for(int i = 0; i < width / 2; i++)
+		{
+			for(int j = 0; j < height / 2; j++)
+			{
+				for(int k = depth / 2; k < depth; k++)
+				{
+					Vector3 e = audio_source.transform.position - point_mass[i, j, k].position;
+					float l = Vector3.Magnitude(e); // confirmed functioning
+					e.Normalize();
+
+					float force = sound_force_calculations(point_mass[i, j, k].position, frequency_groups[1], l);
+					point_mass[i, j, k].rb.velocity +=  (force / mass) * e * delta_time;
+				}
+			}
+		} 
+
+		// octant 2, front lower right
+		for(int i = width / 2; i < width; i++)
+		{
+			for(int j = 0; j < height / 2; j++)
+			{
+				for(int k = 0; k < depth / 2; k++)
+				{
+					Vector3 e = audio_source.transform.position - point_mass[i, j, k].position;
+					float l = Vector3.Magnitude(e); // confirmed functioning
+					e.Normalize();
+
+					float force = sound_force_calculations(point_mass[i, j, k].position, frequency_groups[2], l);
+					point_mass[i, j, k].rb.velocity +=  (force / mass) * e * delta_time;
+				}
+			}
+		}
+
+		// octant 3, back lower right
+		for(int i = width / 2; i < width; i++)
+		{
+			for(int j = 0; j < height / 2; j++)
+			{
+				for(int k = depth / 2; k < depth; k++)
+				{
+					Vector3 e = audio_source.transform.position - point_mass[i, j, k].position;
+					float l = Vector3.Magnitude(e); // confirmed functioning
+					e.Normalize();
+
+					float force = sound_force_calculations(point_mass[i, j, k].position, frequency_groups[3], l);
+					point_mass[i, j, k].rb.velocity +=  (force / mass) * e * delta_time;
+				}
+			}
+		}
+
+		// octant 4, front top left
+		for(int i = 0; i < width / 2; i++)
+		{
+			for(int j = height / 2; j < height; j++)
+			{
+				for(int k = 0; k < depth / 2; k++)
+				{
+					Vector3 e = audio_source.transform.position - point_mass[i, j, k].position;
+					float l = Vector3.Magnitude(e); // confirmed functioning
+					e.Normalize();
+
+					float force = sound_force_calculations(point_mass[i, j, k].position, frequency_groups[0], l);
+					point_mass[i, j, k].rb.velocity +=  (force / mass) * e * delta_time;
+				}
+			}
+		}
+
+		// octant 5, back top left
+		for(int i = 0; i < width / 2; i++)
+		{
+			for(int j = height / 2; j < height; j++)
+			{
+				for(int k = depth / 2; k < depth; k++)
+				{
+					Vector3 e = audio_source.transform.position - point_mass[i, j, k].position;
+					float l = Vector3.Magnitude(e); // confirmed functioning
+					e.Normalize();
+
+					float force = sound_force_calculations(point_mass[i, j, k].position, frequency_groups[1], l);
+					point_mass[i, j, k].rb.velocity +=  (force / mass) * e * delta_time;
+				}
+			}
+		} 
+
+		// octant 6, front lower right
+		for(int i = width / 2; i < width; i++)
+		{
+			for(int j = height / 2; j < height; j++)
+			{
+				for(int k = 0; k < depth / 2; k++)
+				{
+					Vector3 e = audio_source.transform.position - point_mass[i, j, k].position;
+					float l = Vector3.Magnitude(e); // confirmed functioning
+					e.Normalize();
+
+					float force = sound_force_calculations(point_mass[i, j, k].position, frequency_groups[2], l);
+					point_mass[i, j, k].rb.velocity +=  (force / mass) * e * delta_time;
+				}
+			}
+		}
+
+		// octant 7, back lower right
+		for(int i = width / 2; i < width; i++)
+		{
+			for(int j = height / 2; j < height; j++)
+			{
+				for(int k = depth / 2; k < depth; k++)
+				{
+					Vector3 e = audio_source.transform.position - point_mass[i, j, k].position;
+					float l = Vector3.Magnitude(e); // confirmed functioning
+					e.Normalize();
+
+					float force = sound_force_calculations(point_mass[i, j, k].position, frequency_groups[3], l);
+					point_mass[i, j, k].rb.velocity +=  (force / mass) * e * delta_time;
+				}
+			}
+		}
+
 
 
 		// assign positions based on velocities
@@ -252,10 +414,31 @@ Also might want to consider Implicit Euler instead.
 		AudioClip functions will then be useful https://docs.unity3d.com/ScriptReference/AudioClip.html
 		AudioClip.frequency only gives the frequency everything was recorded in, not the sound data shit...
 	*/
-	private float sound_force_calculations(Vector3 point_position)
+
+	private float sound_force_calculations(Vector3 position, float frequency, float l)
 	{
-		
-		return 0.0f;
+		return -1.0f * frequency / l;
+	}
+
+	private void determine_frequency_groups()
+	{
+		// Probably need to use GetSpectrumData https://www.youtube.com/watch?v=4Av788P9stk
+		int count = 0;
+		for(int i = 0; i < 4; i++) // maybe need to do i < 5?
+		{
+			float average = 0.0f;
+			int sample_count = (int) Mathf.Pow(2, i) * 16;
+
+			for(int j = 0; j < sample_count; j++)
+			{
+				average += spectrum_samples[count] * (count + 1);
+				count++;
+			}
+
+			average /= count;
+			frequency_groups[i] = average * 10;
+			Debug.Log("frequency group " + i + ": " + frequency_groups[i]);
+		}
 	}
 
 	void to_string()
